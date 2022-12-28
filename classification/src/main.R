228455b7c5ccb5cc6f75a84172e190e04b1a6e42
Marco Bogani <marco.boga99@gmail.com>
2022-12-28 17:52:56 +0100

    K-Fold Cross Validation

--- classification/src/main.R
+++ classification/src/main.R
@@ -64,7 +64,7 @@ plot_3d <- FALSE
 df <- read.table("data/raw/data.csv", header = T, sep = ",")
 
 df$diagnosis <- as.factor(df$diagnosis)
-df.data <- df[, -1]
+df$data <- df[,-1]
 df.features <- df[, c(3:32)]
 
 if (plot_data) {
@@ -75,7 +75,7 @@ if (plot_data) {
 # Data analisys ========================
 
 if (plot_data) {
-  analisys <- df.data %>%
+  analisys <- df$data %>%
     group_by(diagnosis) %>% # Variable to be transformed
     count() %>%
     ungroup() %>%
@@ -106,7 +106,7 @@ if (plot_data) {
   if (plot_heavy) {
     # Mean group
     ggpairs(
-      df.data,
+      df$data,
       columns = 2:11,
       mapping = aes(color = as.factor(df$diagnosis)),
       upper = list(continuous = wrap("cor", size = 6)),
@@ -115,7 +115,7 @@ if (plot_data) {
     
     # Se group
     ggpairs(
-      df.data,
+      df$data,
       columns = 12:21,
       mapping = aes(color = as.factor(df$diagnosis)),
       upper = list(continuous = wrap("cor", size = 6)),
@@ -124,7 +124,7 @@ if (plot_data) {
     
     # Worst group
     ggpairs(
-      df.data,
+      df$data,
       columns = 22:31,
       mapping = aes(color = as.factor(df$diagnosis)),
       upper = list(continuous = wrap("cor", size = 6)),
@@ -138,9 +138,9 @@ if (plot_data) {
 
 if (clean_data) {
   for (i in c("area_worst", "area_mean", "area_se", "perimeter_worst")) {
-    df.data = remove_outliers(df.data, i)
+    df$data = remove_outliers(df$data, i)
   }
-  boxplotF("No Outliers", df.data[, -1])
+  boxplotF("No Outliers", df$data[,-1])
 }
 
 
@@ -149,7 +149,7 @@ if (clean_data) {
 if (plot_data) {
   corrplot(
     title = "Features correlation",
-    cor(df.data[,-1]),
+    cor(df$data[, -1]),
     diag = FALSE,
     method = "square",
     order = "FPC",
@@ -163,16 +163,16 @@ if (plot_data) {
 
 sample <-
   sample(c(TRUE, FALSE),
-         nrow(df.data),
+         nrow(df$data),
          replace = TRUE,
          prob = c(0.8, 0.2))
-train_set <- df.data[sample, ]
-test_set <- df.data[!sample, ]
+train_set <- df$data[sample,]
+test_set <- df$data[!sample,]
 
 
 # Feature selection =====================
 
-pr_comp <- prcomp(train_set[,-1], scale = TRUE, center = TRUE)
+pr_comp <- prcomp(train_set[, -1], scale = TRUE, center = TRUE)
 
 std_dev <- pr_comp$sdev
 pr_var <- std_dev ^ 2
@@ -190,7 +190,7 @@ train_set$pca <-
 if (plot_data) {
   ## Corr plot
   show(ggcorr(
-    cbind(train_set[,-1], pr_comp$x[, c(1:10)]),
+    cbind(train_set[, -1], pr_comp$x[, c(1:10)]),
     label = TRUE,
     cex = 2.5
   ))
@@ -292,7 +292,7 @@ show(
 
 
 # Test ==================================
-test_prcomp <- prcomp(test_set[,-1], scale = TRUE, center = TRUE)
+test_prcomp <- prcomp(test_set[, -1], scale = TRUE, center = TRUE)
 test_set$pca <-
   data.frame(diagnosis = test_set$diagnosis, test_prcomp$x) [, 1:11]
 
@@ -326,3 +326,63 @@ if (plot_data) {
     )
   )
 }
+
+
+# K-fold Cross Validation ================
+
+pr_comp <- prcomp(df$data[, -1], scale = TRUE, center = TRUE)
+df$pca <-
+  data.frame(diagnosis = df$data$diagnosis, pr_comp$x) [, 1:11]
+
+
+# Create a trainControl object to control how the train function creates the model
+train_control <-
+  trainControl(method = "cv",
+               number = 5)
+
+# Set required parameters for the model type we are using**
+tune_grid = expand.grid(cp = c(0.001))
+
+
+# Use the train() function to create the model
+validated_tree <-
+  train(
+    diagnosis ~ PC1 + PC2 + PC3 + PC4 + PC5 + PC6 + PC7 + PC8 + PC9 + PC10,
+    data = df$pca,
+    # Data set
+    method = "rpart",
+    # Model type(decision tree)
+    trControl = train_control,
+    
+  )
+
+validated_tree         # View a summary of the model
+trellis.par.set(caretTheme())
+show(
+  rpart.plot(
+    validated_tree$finalModel,
+    type = 1,
+    extra = 100,
+    box.palette = "-RdYlGn",
+    branch.lty = 2
+  )
+)
+
+validated_tree$pred <-
+  predict(validated_tree$finalModel,
+          newdata = test_set$pca,
+          type = "class")
+print("=== CLASSIFICATION TREE (k-fold) ===")
+
+show(
+  confusionMatrix(
+    data = validated_tree$pred,
+    reference = test_set$pca$diagnosis,
+    positive = "M"
+  )
+)
+
+validated_tree$prob <-
+  predict(validated_tree$finalModel,
+          newdata = test_set$pca,
+          type = "prob")
